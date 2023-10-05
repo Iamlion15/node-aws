34359c0a0892157b3b462c28c3d4ffadca5f9d18
Iam lion <Iamlion15@gmail.com>
2023-10-05 05:12:27 -0700

    added ses email verification

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,39 @@
+const AWS = require("aws-sdk")
+
+AWS.config.update({
+    accesskeyid: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+})
+const ses = new AWS.SES({ apiVersion: "2010-12-01" })
 exports.register = (req, res) => {
-    console.log(req.body)
-    res.status(200).json(req.body);
+    const { name, email, password } = req.body;
+    const params = {
+        Source: process.env.EMAIL_FROM,
+        Destination: {
+            ToAddresses: [email]
+        },
+        ReplyToAddresses: [process.env.EMAIL_FROM],
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: `<html><body><h1>hello ${name}</h1><p style="color:red;">Test email</p></body></html>`
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Complete your registration'
+            }
+        }
+    }
+    const sendEmailOnRegistration = ses.sendEmail(params).promise()
+    sendEmailOnRegistration.then(data => {
+        console.log("email submitted on SES ", data)
+        res.send("Email sent")
+    })
+        .catch(error => {
+            console.log("ses failed to send email", error)
+            res.send("email failed")
+        })
 }
